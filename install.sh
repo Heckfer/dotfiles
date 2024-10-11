ed0471c1f8e4c318dfd4ed7b560a1dd03ce16a15
Fernando Heck <fernando.heck@wendys.com>
2024-10-11 13:52:18 -0300

    Update install.sh
    - Remove old things
    - Update how we handle the functions

--- install.sh
+++ install.sh
@@ -2,15 +2,12 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-#set -o xtrace
 
 
 echo "== Install =="
-
 echo "- Docker - https://download.docker.com/mac/stable/Docker.dmg"
 echo "- 1Password - https://c.1password.com/dist/1P/mac4"
 echo "- Datagrip - https://download-cf.jetbrains.com/datagrip"
-echo "- Charles - https://www.charlesproxy.com"
 echo "- Sublime - https://download.sublimetext.com"
 echo "- VSCode"
 echo "- Android Studio"
@@ -29,7 +26,7 @@ echo "[ -d ~/.oh-my-zsh ] || git clone https://github.com/robbyrussell/oh-my-zsh
 echo "[ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ] || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 echo "[ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ] || git clone git@github.com:zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
 echo "[ -d ~/.oh-my-zsh/themes/dracula.zsh-theme ] || git clone git@github.com:dracula/zsh.git ~/.oh-my-zsh/themes/dracula.zsh-theme"
-echo "ASDF (elixir, elm, erlang, haskell, java, kotlin, nodejs, python, ruby)"
+echo "ASDF (java, nodejs, python, ruby, flutter)"
 
 echo "Symlinking dotfiles..."
 ln -sf $(pwd)/vim/.vimrc ~/.vimrc
@@ -37,7 +34,5 @@ ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/git/.gitignore_global ~/.gitignore_global
 ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
 ln -sf $(pwd)/zsh/.zshenv ~/.zshenv
-mkdir ~/.heck
-ln -sf $(pwd)/sh/.utility_functions ~/.heck/.utility_functions
 
 chsh -s $(which zsh)
