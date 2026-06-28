ac4530c9f9a12bea23fc20d2d2451216e6b7c4f4
Fernando Heck <heckfer@gmail.com>
2026-06-28 11:02:37 -0300

    Remove old language versions from configure.sh, remove elm plugin

--- configure.sh
+++ configure.sh
@@ -3,39 +3,26 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-#set -o xtrace
 
 echo "configuring..."
 
 echo "java plugin..."
-asdf plugin-add java
-asdf install java oracle-8.141
-
-echo "elm plugin..."
-asdf plugin-add elm https://github.com/vic/asdf-elm.git
-asdf install elm 0.18.0
+asdf plugin add java
 
 echo "kotlin plugin..."
-asdf plugin-add kotlin https://github.com/missingcharacter/asdf-kotlin
-asdf install kotlin 1.3.21
+asdf plugin add kotlin
 
 echo "python plugin..."
-asdf plugin-add python https://github.com/danhper/asdf-python
-asdf install python 3.7.3
+asdf plugin add python
 
 echo "ruby plugin..."
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby
-asdf install ruby 2.5.3
-asdf install ruby 2.6.2
+asdf plugin add ruby
 
 echo "erlang plugin..."
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf install erlang 21.3.3
+asdf plugin add erlang
 
 echo "elixir plugin..."
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-asdf install elixir 1.7.4-otp-21
-asdf install elixir 1.8.1-otp-21
+asdf plugin add elixir
 
 echo "finished installing utilities"
 
